000d4548cd97f147d407409d730d690659289892
X_AJ_X <88792057+XAJX179@users.noreply.github.com>
2025-10-24 23:48:40 +0530

    docs(user): remove useless comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,5 @@
 class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true, length: 4..20
 
-  # Some databases are configured to perform case-insensitive searches anyway.
-  # source: https://guides.rubyonrails.org/active_record_validations.html#uniqueness
   validates :email, presence: true, uniqueness: true
 end
